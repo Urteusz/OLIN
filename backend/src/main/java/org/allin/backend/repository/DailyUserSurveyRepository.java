8cae84e251c2ec42420325e66a5027c46c81bce6
Nikodem Nowak <71512463+NikodemNowak@users.noreply.github.com>
2025-05-17 00:34:16 +0200

    Add endpoint to retrieve daily user surveys by username

--- backend/src/main/java/org/allin/backend/repository/DailyUserSurveyRepository.java
+++ backend/src/main/java/org/allin/backend/repository/DailyUserSurveyRepository.java
@@ -3,9 +3,11 @@ package org.allin.backend.repository;
 import org.allin.backend.model.DailyUserSurvey;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface DailyUserSurveyRepository extends JpaRepository<DailyUserSurvey, UUID> {
     Optional<DailyUserSurvey> findByUserId(UUID userId);
+    List<DailyUserSurvey> findAllByUserUsername(String username);
 }
